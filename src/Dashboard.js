320c5e3daca9b9066d76a6d19d0b4d527b18e4cb
Taro <jollypumpkin@mailbox.org>
2020-02-23 16:03:17 +0200

    Display the results
    -Introduce Ant Design
    -Added Collapse and Timeline elements
    -Added functions that convert data in human-readable format

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { Collapse, Timeline } from "antd";
+import "antd/dist/antd.css";
 import "./Dashboard.css";
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: null };
+    this.state = {};
   }
 
   fireRequest() {
@@ -15,7 +16,7 @@ class Dashboard extends Component {
     const request = `
       {
         plan(
-          from: {lat: 60.168992, lon: 24.932366}
+          from: {lat: 60.234504, lon: 25.012588}
           to: {lat: 60.169403, lon: 24.925805}
           numItineraries: 6
         ) {
@@ -47,20 +48,81 @@ class Dashboard extends Component {
         return data.json();
       })
       .then(res => {
-        this.state.data = res;
-        console.log(res);
+        this.setState({ response: res });
+        // console.log(res);
       })
       .catch(error => console.log(error));
   }
 
   componentDidMount() {
+    this.fireRequest();
     setInterval(() => {
       this.fireRequest();
     }, 60000);
   }
 
+  getDisplayTime(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    let formattedTime = hours + ":" + minutes.substr(-2);
+    return formattedTime;
+  }
+
+  getDisplayDistance(distance) {
+    return Math.ceil(distance / 100) / 10;
+  }
+
+  getDisplayDuration(duration) {
+    return Math.ceil(duration / 60);
+  }
+
+  getTripDuration(trip) {
+    return this.getDisplayDuration(
+      trip.legs.map(leg => leg.duration).reduce((a, b) => a + b)
+    );
+  }
+
   render() {
-    return <div className="HelloWorld">{this.state.data}</div>;
+    const { Panel } = Collapse;
+
+    function callback(key) {}
+
+    let collapse;
+    if (this.state.response) {
+      collapse = (
+        <div>
+          {this.state.response.data.plan.itineraries.map((it, it_index) => (
+            <Collapse defaultActiveKey={[it_index]} onChange={callback}>
+              <Panel
+                header={
+                  this.getDisplayTime(it.legs[0].startTime) +
+                  " " +
+                  this.getTripDuration(it) +
+                  "m"
+                }
+                key={it_index}
+              >
+                <Timeline>
+                  {it.legs.map((l, leg_index) => (
+                    <Timeline.Item>
+                      {leg_index}
+                      Start Time: {this.getDisplayTime(l.startTime)}
+                      Distance: {this.getDisplayDistance(l.distance)}
+                      Mode: {l.mode}
+                      Duration: {this.getDisplayDuration(l.duration)}
+                    </Timeline.Item>
+                  ))}
+                </Timeline>
+              </Panel>
+            </Collapse>
+          ))}
+        </div>
+      );
+    } else {
+      collapse = <div>else</div>;
+    }
+    return <div>{collapse}</div>;
   }
 }
 
